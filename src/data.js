5bcdd7934a32b06bf28c6bc4639e003036c7bf42
Samah Bani Odeh <110301925+sammahkh@users.noreply.github.com>
2025-05-03 14:38:37 +0000

    Initial Setup

---
+++ src/data.js
@@ -0,0 +1,102 @@
+const list = [
+  {
+    id: 0,
+    name: "iPhone 6S",
+    description:
+      "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+    imageUrl: "http://www.icentar.me/phone/6s/images/goldbig.jpg",
+    price: 799,
+  },
+  {
+    id: 1,
+    name: "iPhone 5S",
+    description:
+      "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+    imageUrl: "http://www.icentar.me/phone/5s/images/silverbig.png",
+    price: 349,
+  },
+  {
+    id: 2,
+    name: "Macbook",
+    description:
+      "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+    imageUrl: "http://www.icentar.me/mac/macbook/images/pro.jpg",
+    price: 1499,
+  },
+  {
+    id: 3,
+    name: "Macbook Air",
+    description:
+      "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+    imageUrl: "http://www.icentar.me/mac/mbair/images/air.jpg",
+    price: 999,
+  },
+  {
+    id: 4,
+    name: "Macbook Air 2013",
+    description:
+      "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+    imageUrl: "http://www.icentar.me/mac/mbair/images/air.jpg",
+    price: 599,
+  },
+  {
+    id: 5,
+    name: "Macbook Air 2012",
+    description:
+      "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+    imageUrl: "http://www.icentar.me/mac/mbair/images/air.jpg",
+    price: 499,
+  },
+  [
+    {
+      id: 6,
+      name: "iPhone 6S",
+      description:
+        "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+      imageUrl: "http://www.icentar.me/phone/6s/images/goldbig.jpg",
+      price: 799,
+    },
+    {
+      id: 7,
+      name: "iPhone 5S",
+      description:
+        "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+      imageUrl: "http://www.icentar.me/phone/5s/images/silverbig.png",
+      price: 349,
+    },
+    {
+      id: 8,
+      name: "Macbook",
+      description:
+        "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+      imageUrl: "http://www.icentar.me/mac/macbook/images/pro.jpg",
+      price: 1499,
+    },
+    {
+      id: 9,
+      name: "Macbook Air",
+      description:
+        "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+      imageUrl: "http://www.icentar.me/mac/mbair/images/air.jpg",
+      price: 999,
+    },
+    {
+      id: 10,
+      name: "Macbook Air 2013",
+      description:
+        "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+      imageUrl: "http://www.icentar.me/mac/mbair/images/air.jpg",
+      price: 599,
+    },
+    {
+      id: 11,
+      name: "Macbook Air 2012",
+      description:
+        "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+      imageUrl: "http://www.icentar.me/mac/mbair/images/air.jpg",
+      price: 499,
+    },
+  ],
+];
+
+export default list;
